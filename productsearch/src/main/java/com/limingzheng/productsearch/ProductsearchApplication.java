036c22ab0585b9a8df88a1fa8a1d2823f7c9154f
liming <zlm17unl@gmail.com>
2025-06-12 17:05:03 -0400

    add initial files

---
+++ productsearch/src/main/java/com/limingzheng/productsearch/ProductsearchApplication.java
@@ -0,0 +1,13 @@
+package com.limingzheng.productsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsearchApplication.class, args);
+	}
+
+}
---
+++ productsearch/src/test/java/com/limingzheng/productsearch/ProductsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.limingzheng.productsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
