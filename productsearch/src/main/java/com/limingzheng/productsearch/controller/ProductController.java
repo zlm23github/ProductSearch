7e74d2f87300e8473228d76a9c264b87ef641bd0
liming <zlm17unl@gmail.com>
2025-06-13 14:54:27 -0400

    implemented ProductController & ProductService class

--- productsearch/src/main/java/com/limingzheng/productsearch/controller/ProductController.java
+++ productsearch/src/main/java/com/limingzheng/productsearch/controller/ProductController.java
@@ -1,13 +1,31 @@
 package com.limingzheng.productsearch.controller;
 
+import com.limingzheng.productsearch.entity.Product;
 import com.limingzheng.productsearch.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/products")
 public class ProductController {
-    @Autowired
-    private ProductService productService;
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable Long id) {
+        return productService.getProductById(id);
+    }
 }
--- productsearch/src/main/java/com/limingzheng/productsearch/service/ProductService.java
+++ productsearch/src/main/java/com/limingzheng/productsearch/service/ProductService.java
@@ -1,12 +1,26 @@
 package com.limingzheng.productsearch.service;
 
+import com.limingzheng.productsearch.entity.Product;
 import com.limingzheng.productsearch.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ProductService {
-    @Autowired
-    private ProductRepository productRepository;
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
 
 }
