bdda08c35b6917e05d8c300ae1e4294f35c386ae
liming <zlm17unl@gmail.com>
2025-06-13 15:42:53 -0400

    add Junit test to test APIs

--- productsearch/src/test/java/com/limingzheng/productsearch/ProductsearchApplicationTests.java
+++ productsearch/src/test/java/com/limingzheng/productsearch/ProductsearchApplicationTests.java
@@ -1,13 +1,32 @@
 package com.limingzheng.productsearch;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ProductsearchApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	public void testGetAllProducts() throws Exception {
+		mockMvc.perform(get("/api/products"))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void testGetProductById() throws Exception {
+		mockMvc.perform(get("/api/products/1"))
+				.andExpect(status().isOk());
 	}
 
 }
