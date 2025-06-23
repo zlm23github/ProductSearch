88f9424636ec9cb49ec0d69c636aa53bb8e55260
liming <zlm17unl@gmail.com>
2025-06-23 15:24:07 -0400

    Add exception handling logic to catch and log errors during the search operation.

--- productsearch/src/main/java/com/limingzheng/productsearch/es/service/ProductESService.java
+++ productsearch/src/main/java/com/limingzheng/productsearch/es/service/ProductESService.java
@@ -5,6 +5,7 @@ import co.elastic.clients.elasticsearch._types.query_dsl.*;
 import co.elastic.clients.json.JsonData;
 import com.limingzheng.productsearch.es.entity.ProductDocument;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -20,65 +21,73 @@ import java.util.ArrayList;
 
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class ProductESService {
 
     private final ElasticsearchOperations elasticsearchOperations;
 
     public Page<ProductDocument> searchProducts(String keyword, String category, Double minPrice, Double maxPrice, Pageable pageable) {
-        //create BoolQuery builder
-        BoolQuery.Builder boolQueryBuilder = new BoolQuery.Builder();
+        try{
+            //create BoolQuery builder
+            BoolQuery.Builder boolQueryBuilder = new BoolQuery.Builder();
 
-        if (StringUtils.hasText(keyword)) {
-            MultiMatchQuery multiMatchQuery = new MultiMatchQuery.Builder()
-                    .query(keyword)
-                    .fields("title", "description")
-                    .build();
-            boolQueryBuilder.must(multiMatchQuery._toQuery());
-        }
-
-        if (StringUtils.hasText(category)) {
-            TermQuery termQuery = new TermQuery.Builder()
-                    .field("category.keyword")
-                    .value(category)
-                    .build();
-            boolQueryBuilder.filter(termQuery._toQuery());
-        }
-
-        if (minPrice != null || maxPrice != null) {
-            RangeQuery.Builder rangeQueryBuilder = new RangeQuery.Builder().field("price");
-            if (minPrice != null) {
-                rangeQueryBuilder.gte(JsonData.of(minPrice));
+            if (StringUtils.hasText(keyword)) {
+                MultiMatchQuery multiMatchQuery = new MultiMatchQuery.Builder()
+                        .query(keyword)
+                        .fields("title", "description")
+                        .build();
+                boolQueryBuilder.must(multiMatchQuery._toQuery());
             }
-            if (maxPrice != null) {
-                rangeQueryBuilder.lte(JsonData.of(maxPrice));
+
+            if (StringUtils.hasText(category)) {
+                TermQuery termQuery = new TermQuery.Builder()
+                        .field("category.keyword")
+                        .value(category)
+                        .build();
+                boolQueryBuilder.filter(termQuery._toQuery());
             }
-            boolQueryBuilder.filter(rangeQueryBuilder.build()._toQuery());
+
+            if (minPrice != null || maxPrice != null) {
+                RangeQuery.Builder rangeQueryBuilder = new RangeQuery.Builder().field("price");
+                if (minPrice != null) {
+                    rangeQueryBuilder.gte(JsonData.of(minPrice));
+                }
+                if (maxPrice != null) {
+                    rangeQueryBuilder.lte(JsonData.of(maxPrice));
+                }
+                boolQueryBuilder.filter(rangeQueryBuilder.build()._toQuery());
+            }
+
+            //create final BoolQuery object
+            BoolQuery finalBoolQuery = boolQueryBuilder.build();
+
+            Query finalQuery;
+            if (!finalBoolQuery.hasClauses()) {
+                finalQuery = new MatchAllQuery.Builder().build()._toQuery();
+            } else {
+                finalQuery = finalBoolQuery._toQuery();
+            }
+
+            //Build a local query and include pagination details
+            NativeQuery nativeQuery = NativeQuery.builder()
+                    .withQuery(finalQuery)
+                    .withPageable(pageable)
+                    .build();
+
+            SearchHits<ProductDocument> searchHits = elasticsearchOperations.search(nativeQuery, ProductDocument.class);
+
+            List<ProductDocument> productList = new ArrayList<>();
+            for (SearchHit<ProductDocument> hit : searchHits) {
+                productList.add(hit.getContent());
+            }
+
+            return new PageImpl<>(productList, pageable, searchHits.getTotalHits());
+
+        }catch (Exception e) {
+            log.error("Elasticsearch search failed!", e);
+            throw new RuntimeException("Elasticsearch search failed", e);
         }
 
-        //create final BoolQuery object
-        BoolQuery finalBoolQuery = boolQueryBuilder.build();
-
-        Query finalQuery;
-        if (!finalBoolQuery.hasClauses()) {
-            finalQuery = new MatchAllQuery.Builder().build()._toQuery();
-        } else {
-            finalQuery = finalBoolQuery._toQuery();
-        }
-
-        //Build a local query and include pagination details
-        NativeQuery nativeQuery = NativeQuery.builder()
-                .withQuery(finalQuery)
-                .withPageable(pageable)
-                .build();
-
-        SearchHits<ProductDocument> searchHits = elasticsearchOperations.search(nativeQuery, ProductDocument.class);
-
-        List<ProductDocument> productList = new ArrayList<>();
-        for (SearchHit<ProductDocument> hit : searchHits) {
-            productList.add(hit.getContent());
-        }
-
-        return new PageImpl<>(productList, pageable, searchHits.getTotalHits());
     }
 }
--- productsearch/src/main/java/com/limingzheng/productsearch/service/ProductService.java
+++ productsearch/src/main/java/com/limingzheng/productsearch/service/ProductService.java
@@ -2,6 +2,7 @@ package com.limingzheng.productsearch.service;
 
 import com.limingzheng.productsearch.entity.Product;
 import com.limingzheng.productsearch.repository.ProductRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
+@Slf4j
 public class ProductService {
     private final ProductRepository productRepository;
 
@@ -18,7 +20,13 @@ public class ProductService {
     }
 
     public Page<Product> searchProducts(String keyword, String category, Double minPrice, Double maxPrice, Pageable pageable) {
-        return productRepository.searchProducts(keyword, category, minPrice, maxPrice, pageable);
+        try{
+            return productRepository.searchProducts(keyword, category, minPrice, maxPrice, pageable);
+        } catch(Exception e) {
+            log.error("Elasticsearch search failed!", e);
+            throw new RuntimeException("Elasticsearch search failed", e);
+        }
+
     }
 
 }
